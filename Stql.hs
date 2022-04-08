9619b5435e676246810dea0875410eae9833feed
Patricia <pww1u19@soton.ac.uk>
2022-04-09 00:35:54 +0200

    chore: some modules + testing the git bot

--- Stql.hs
+++ Stql.hs
@@ -5,5 +5,11 @@ Yaqin Hasan
 Patrycja Wanat
 -}
 
+-- module Main(main) where   -- the main module
+-- module Lexer (lexer) where
+-- import Control.Monad.State
+-- import Control.Monad.Error
+-- import Data.Word
+
 main :: IO ()
 main = putStrLn "Hello, World!"
