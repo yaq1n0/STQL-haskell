68880480b05c6602c4d17e530de715ad9408f609
Patricia <pww1u19@soton.ac.uk>
2022-04-12 02:08:04 +0200

    feat: add full triples import in Rdf4h and Swish

--- app/Stql.hs
+++ app/Stql.hs
@@ -9,6 +9,11 @@ Patrycja Wanat
 
 module Main where
 
+import System.IO  
+import Control.Monad
+import qualified Data.Text as T
+import Data.Text.Lazy as TL
+
 import Swish.RDF.Parser.Turtle
     ( 
       ParseResult,
@@ -29,35 +34,23 @@ import Data.RDF
     query,
     objectOf
   )
-import qualified Data.Text as T
 
-timBernersLee :: IO ()
-timBernersLee = do
+-- Swish version
+importFile :: IO String
+importFile = do
+      contents <- readFile "../inputs/bar.ttl"
+      let res = parseInSwish contents
+      print res
+      return contents
+
+parseInSwish :: String -> ParseResult
+parseInSwish turtleString = parseTurtle (TL.pack turtleString) Nothing
+
+-- Rdf4h version
+parseInRdf4 :: IO ()
+parseInRdf4 = do
   result <- parseFile (TurtleParser Nothing Nothing) "../inputs/bar.ttl" :: IO (Either ParseFailure (RDF TList))
   print result
 
-lol :: ParseResult
-lol = parseTurtle "<http://www.cw.org/subjectA> <http://www.cw.org/predicateA> <http://www.cw.org/objectA> ." Nothing
-
 main :: IO ()
-main = timBernersLee
-
--- main :: IO TList
--- main = fmap fromEither (parseFile TurtleParser "../inputs/bar.ttl")
--- rdfGraph1 :: IO TriplesList
--- rdfGraph1 = fmap fromEither (parseFile TurtleParser "../inputs/bar.ttl")
-
--- rdfGraph2 :: IO TriplesList
--- rdfGraph2 = fmap fromEither (parseFile TurtleParser "../inputs/foo.ttl")
-
--- example :: IO ()
--- example = do
---   g1 <- rdfGraph1
---   g2 <- rdfGraph2
---   let node1 = lnode $ PlainL "foo"
---   putStrLn $ "Subjects of g1: " ++ show (map subjectOf (triplesOf g1))
---   putStrLn $ "RDF contains literal 'foo': " ++ show (rdfContainsNode g1 node1)
---   putStrLn $ "Isomorphism test: " ++ show (isIsomorphic g1 g2)
---   putStrLn $ "Unsorted triples: " ++ show (triplesOf g2)
---   putStrLn $ "Sorted triples: "   ++ show ((sort . triplesOf) g2)
---   putStrLn $ "Query: " ++ show (query g1 Nothing Nothing (Just node1))
+main = parseInRdf4
