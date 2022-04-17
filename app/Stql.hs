178ed522678d119451a38d41bdfe2ea803fbd697
Patricia <pww1u19@soton.ac.uk>
2022-04-18 00:25:20 +0200

    feat/chore: add formatting graph from text + tidy up imports

--- app/Stql.hs
+++ app/Stql.hs
@@ -1,49 +1,10 @@
-import Swish.RDF.Parser.Turtle
-  (
-    ParseResult,
-    parseTurtle
-    -- parseTurtlefromText  
-  )
-
-import Swish.Monad
-  (
-    SwishState,
-    SwishStateIO,
-    emptyState,
-    setFormat,
-    SwishFormat (Turtle)
-  )
-
--- import Data.Text
-
-import Data.Text.Lazy as TL 
-  (
-    Text,
-    pack
-  )
-
-import Swish.RDF.Graph
-    (
-      RDFGraph,
-      emptyRDFGraph,
-      toRDFLabel
-    )
-
-import Swish.RDF.Formatter.Turtle
-    (
-      formatGraphAsText
-    )
-
-import Swish.Commands
-  (
-    swishInput
-  )
-
-import Control.Monad.State.Lazy
-  (
-    get,
-    return
-  )
+import Swish.RDF.Parser.Turtle (ParseResult, parseTurtle)
+import Swish.Monad (SwishState, SwishStateIO, emptyState, setFormat, SwishFormat (Turtle))
+import Data.Text.Lazy as TL (Text, pack, unpack)
+import Swish.RDF.Graph (RDFGraph, emptyRDFGraph, toRDFLabel)
+import Swish.RDF.Formatter.Turtle (formatGraphAsText)
+import Swish.Commands (swishInput)
+import Control.Monad.State.Lazy (get, return, liftIO)
 
 createGraph :: RDFGraph
 createGraph = emptyRDFGraph
@@ -52,25 +13,41 @@ main :: IO ()
 main = putStrLn "hello, swish!"
 
 -- Swish version
-importFile :: IO String
+-- importFile :: IO String
 importFile = do
       contents <- readFile "../inputs/bar.ttl"
-      let j = swishInput (Just contents)
-      -- putStrLn (return j)
-      putStrLn contents
-      -- res <- parseInSwish contents
-      -- case contents of
-      --   Left err -> error "Can't parse the file."
-      --   Right rdfGraph -> do
-      --       let o = formatGraphAsText rdfGraph
-      --       print o
-      return contents
+      
+      
+      -- putStrLn contents
+      case (parseIntoTurtle contents) of
+        Left err -> error "Can't parse the file."
+        Right rdfGraph -> do
+            let o = formatGraphAsText rdfGraph
+            print o
+      
+      putStrLn "\n"
+      let printComment = "RETURNING: " ++ contents
+      return printComment
+
+-- readInGraph :: SwishStateIO ()
+-- readInGraph = do
+--           lol <- importFile
+--           swishInput (Just lol)
 
 -- fileToLabel :: String
 -- fileToLabel s = toRDFLabel s
 
--- parseInSwish :: String -> ParseResult
--- parseInSwish turtleString = parseTurtle (TL.pack turtleString) Nothing
+-- changes String into Text
+fromStrToText :: String -> Text
+fromStrToText s = TL.pack s
+
+-- changes Text into String
+fromTextToStr :: Text -> String
+fromTextToStr s = TL.unpack s
+
+-- change input into Text and parse it into the turtle format
+parseIntoTurtle :: String -> ParseResult
+parseIntoTurtle turtleString = parseTurtle (fromStrToText turtleString) Nothing
 
 createState :: SwishState
 createState = do
