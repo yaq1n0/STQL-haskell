c148fbfdb39ba0e2d019473d7d8461db126506a3
Patricia <pww1u19@soton.ac.uk>
2022-04-18 16:28:02 +0200

    feat: add parsing from a turtle file in swish + update README

--- app/Stql.hs
+++ app/Stql.hs
@@ -1,10 +1,13 @@
-import Swish.RDF.Parser.Turtle (ParseResult, parseTurtle)
+import Swish.RDF.Parser.Turtle (ParseResult, parseTurtle, parseTurtlefromText)
 import Swish.Monad (SwishState, SwishStateIO, emptyState, setFormat, SwishFormat (Turtle))
-import Data.Text.Lazy as TL (Text, pack, unpack)
+import qualified Data.Text.Lazy as TL (Text, pack, unpack)
+import qualified Data.Text as T (Text, pack, unpack, stripPrefix, stripSuffix)
 import Swish.RDF.Graph (RDFGraph, emptyRDFGraph, toRDFLabel)
 import Swish.RDF.Formatter.Turtle (formatGraphAsText)
 import Swish.Commands (swishInput)
 import Control.Monad.State.Lazy (get, return, liftIO)
+import Data.List (isPrefixOf, isSuffixOf)
+import Data.Maybe (fromJust)
 
 createGraph :: RDFGraph
 createGraph = emptyRDFGraph
@@ -13,41 +16,77 @@ main :: IO ()
 main = putStrLn "hello, swish!"
 
 -- Swish version
--- importFile :: IO String
+importFile :: IO String
 importFile = do
       contents <- readFile "../inputs/bar.ttl"
-      
-      
       -- putStrLn contents
+
+      -- printGraph $ graphFromFileString contents
+      putStrLn "BASE: "
+      print (getBase contents)
       case (parseIntoTurtle contents) of
-        Left err -> error "Can't parse the file."
-        Right rdfGraph -> do
-            let o = formatGraphAsText rdfGraph
-            print o
+                                  Left err -> putStrLn "Can't parse the file."
+                                  Right rdfGraph -> do
+                                      printGraph rdfGraph
       
       putStrLn "\n"
       let printComment = "RETURNING: " ++ contents
       return printComment
 
--- readInGraph :: SwishStateIO ()
--- readInGraph = do
---           lol <- importFile
---           swishInput (Just lol)
+-- graphFromFileString :: String -> RDFGraph
+-- graphFromFileString contents = do
 
--- fileToLabel :: String
--- fileToLabel s = toRDFLabel s
+-- get the base value from a turtle file 
+getBase :: String -> Maybe String
+getBase content = do
+            let bases = filter isBase $ lines content
+            
+            -- let bases = matchAllBases content
+            
+            case bases of
+              [] -> Nothing
+              [a] -> Just (extractBase a)
+              (a:as) -> error "The document contains too many @base properties (more than one)"
+
+-- extract the actual base string
+extractBase :: String -> String
+extractBase base = do
+            let maybesP = T.stripPrefix (strToText "@base ") (strToText base)
+            let sP = fromJust maybesP
+            let maybesS = T.stripSuffix (strToText " .") sP
+            let sS = fromJust maybesS
+            textToStr sS
+
+isBase :: String -> Bool
+isBase c = "@base " `isPrefixOf` c && " ." `isSuffixOf` c
+
+-- matchAllBases :: String -> [String]
+-- matchAllBases c = getAllTextMatches $ c =~ "(^@base)(.*)( \\.$)"
+-- "(?<=@base )(.*)(?= .)")
+
+-- parse input into the turtle format
+parseIntoTurtle :: String -> ParseResult
+parseIntoTurtle turtleString = parseTurtle (strToLText turtleString) Nothing
+
+-- print Graph into output (with newlines)
+printGraph :: RDFGraph -> IO ()
+printGraph g = putStrLn (textToStr $ formatGraphAsText g)
 
 -- changes String into Text
-fromStrToText :: String -> Text
-fromStrToText s = TL.pack s
+strToText :: String -> T.Text
+strToText c = T.pack c
 
 -- changes Text into String
-fromTextToStr :: Text -> String
-fromTextToStr s = TL.unpack s
+textToStr :: T.Text -> String
+textToStr c = T.unpack c
 
--- change input into Text and parse it into the turtle format
-parseIntoTurtle :: String -> ParseResult
-parseIntoTurtle turtleString = parseTurtle (fromStrToText turtleString) Nothing
+-- changes String into Lazy Text
+strToLText :: String -> TL.Text
+strToLText c = TL.pack c
+
+-- changes Lazy Text into String
+lTextToStr :: TL.Text -> String
+lTextToStr c = TL.unpack c
 
 createState :: SwishState
 createState = do
@@ -56,3 +95,11 @@ createState = do
       
 -- toText :: Text -> ParseResult
 -- toText p = parseTurtle p Nothing
+
+-- readInGraph :: SwishStateIO ()
+-- readInGraph = do
+--           lol <- importFile
+--           swishInput (Just lol)
+
+-- fileToLabel :: String
+-- fileToLabel s = toRDFLabel s
