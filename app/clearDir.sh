9e460553fad6f5cd8b511443bb20fd56ab25d8f0
Patricia <pww1u19@soton.ac.uk>
2022-04-20 21:59:11 +0200

    feat: change the cleaning script to remove all files

--- app/clearDir.sh
+++ app/clearDir.sh
@@ -1,12 +1,4 @@
-rm AST.hi
-rm AST.o
-rm Evaluator.hi
-rm Evaluator.o
-rm Interpreter.hi
-rm Interpreter.o
-rm Lexer.hi
-rm Lexer.o
-rm Parser.hi
-rm Parser.o
-rm Stql.hi
-rm Stql.o
+rm *.hi
+rm *.o
+rm Stql
+rm Stql-Logic
